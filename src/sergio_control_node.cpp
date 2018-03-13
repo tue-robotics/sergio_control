745cf6f15851a5348d80071ed5e0130dd4d7a154
Rein Appeldoorn <rein@ruvu.nl>
2018-03-13 23:04:16 +0100

    Use other frequencies

--- src/sergio_control_node.cpp
+++ src/sergio_control_node.cpp
@@ -41,7 +41,7 @@ int main(int argc, char* argv[])
   ros::init(argc, argv, "sergio_control");
 
   ros::NodeHandle local_nh("~");
-  ros::Rate rate(local_nh.param("frequency", 10));
+  ros::Rate rate(local_nh.param("frequency", 50));
   std::string ethernet_interface = local_nh.param("ethernet_interface", std::string("eno1"));
   std::string urdf_string = local_nh.param("/robot_description", std::string(""));
 
